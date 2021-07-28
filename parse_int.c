8a486cdf9d8150bb747618f503bf6554def19103
David Wang <devtesting404@gmail.com>
2021-07-28 14:54:48 +0800

    removed total precision for parse_int

--- parse_int.c
+++ parse_int.c
@@ -4,13 +4,11 @@ void	parse_int(t_specifier *specifier)
 {
 	char		*str;
 	long long	input;
-	int			total_precision;
 
 	input = va_arg(specifier->args, int);
 	if (input < 0)
 		specifier->flags.prepend_space = 0;
 	str = int_handler(input, specifier, "0123456789");
-	total_precision = ft_strlen(str);
-	width_handler(specifier, str, total_precision);
+	width_handler(specifier, str, ft_strlen(str));
 	free(str);
 }
