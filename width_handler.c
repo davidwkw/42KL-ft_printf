2cc23d9060093932e71e3969cd2ffb12b7d62030
David Wang <devtesting404@gmail.com>
2021-07-29 12:10:50 +0800

    remove redundant pad line from width handler

--- width_handler.c
+++ width_handler.c
@@ -18,8 +18,6 @@ void	width_handler(t_specifier *specifier, char *str, size_t str_len)
 	if (specifier->flags.pad_zero)
 		pad_char = '0';
 	width_diff = specifier->flags.width - str_len - specifier->flags.sign;
-	if (specifier->flags.pad_zero)
-		pad(specifier, pad_char, &width_diff);
 	if (width_diff > 0 && !specifier->flags.pad_right)
 		pad(specifier, pad_char, &width_diff);
 	specifier->nprint += write(1, str, str_len);
