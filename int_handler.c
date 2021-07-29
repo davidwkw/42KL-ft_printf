010c646b80b8704a0bc47b7f11d1b4330fba4951
David Wang <devtesting404@gmail.com>
2021-07-29 22:45:11 +0800

    move sign prepender out of int handler

--- int_handler.c
+++ int_handler.c
@@ -1,29 +1,6 @@
 #include "ft_printf.h"
 
-static char	*sign_selector(t_specifier *specifier, long long input)
-{
-	char	*str;
 
-	if (input < 0)
-		str = ft_strdup("-");
-	else if (specifier->flags.pos)
-		str = ft_strdup("+");
-	else if (specifier->flags.prepend_space)
-		str = ft_strdup(" ");
-	else
-		str = ft_strdup("");
-	return (str);
-}
-
-static char	*prepend_sign(char *sign, char *str)
-{
-	char	*new_str;
-
-	new_str = ft_strjoin(sign, str);
-	free(sign);
-	free(str);
-	return (new_str);
-}
 
 char	*int_handler(long long input, t_specifier *specifier, char *base_str)
 {
@@ -31,7 +8,6 @@ char	*int_handler(long long input, t_specifier *specifier, char *base_str)
 	char			*new_str;
 	char			*initial_str;
 	unsigned int	str_len;
-	char			*sign;
 
 	if (!input && specifier->flags.f_prec)
 		str = ft_strdup("");
@@ -40,13 +16,12 @@ char	*int_handler(long long input, t_specifier *specifier, char *base_str)
 	else
 		str = ft_llutoa_base(input, base_str);
 	str_len = ft_strlen(str);
-	sign = sign_selector(specifier, input);
 	if (specifier->flags.f_prec && str_len < specifier->flags.prec)
 		initial_str = str_create('0', specifier->flags.prec - str_len);
 	else
 		initial_str = ft_strdup("");
-	new_str = prepend_sign(sign, ft_strjoin(initial_str, str));
-	free(str);
+	new_str = ft_strjoin(initial_str, str);
 	free(initial_str);
+	free(str);
 	return (new_str);
 }
--- parse_int.c
+++ parse_int.c
@@ -1,14 +1,43 @@
 #include "ft_printf.h"
 
+static char	*sign_selector(t_specifier *specifier, long long input)
+{
+	char	*str;
+
+	if (input < 0)
+		str = ft_strdup("-");
+	else if (specifier->flags.pos)
+		str = ft_strdup("+");
+	else if (specifier->flags.prepend_space)
+		str = ft_strdup(" ");
+	else
+		str = ft_strdup("");
+	return (str);
+}
+
+static char	*prepend_sign(char *sign, char *str)
+{
+	char	*new_str;
+
+	new_str = ft_strjoin(sign, str);
+	free(sign);
+	free(str);
+	return (new_str);
+}
+
 void	parse_int(t_specifier *specifier)
 {
 	char		*str;
 	long long	input;
+	char		*sign;
+	char		*new_str;
 
 	input = va_arg(specifier->args, int);
 	if (input < 0)
 		specifier->flags.prepend_space = 0;
+	sign = sign_selector(specifier, input);
 	str = int_handler(input, specifier, "0123456789");
+	new_str = prepend_sign(sign, str);
 	width_handler(specifier, str, ft_strlen(str));
 	free(str);
 }
